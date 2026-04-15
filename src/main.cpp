9af24bb1c6257cef409032b5a55b5bd7dfcc315e
TymekBrunka <tymkopolo@wp.pl>
2026-04-15 22:28:47 +0200

    -

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,76 @@
-#include "raylib.h"
-#include "rlImGui.h"
 #include "imgui.h"
 #include "imgui_internal.h"
+#include "raylib.h"
+#include "rlImGui.h"
 
-int main(void)
-{
+int main(void) {
   SetConfigFlags(FLAG_WINDOW_RESIZABLE);
   SetConfigFlags(FLAG_WINDOW_UNDECORATED);
   InitWindow(800, 600, "awaria");
   SetTargetFPS(60);
 
   rlImGuiSetup(true);
-  ImGui::GetIO().ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+  ImGuiIO &io = ImGui::GetIO();
+  io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+  ImFont *font1 = io.Fonts->AddFontFromFileTTF("src/Roboto-Regular.ttf", 30);
+
+  ImGuiStyle &style = ImGui::GetStyle();
+  style.FontSizeBase = 17.0f;
+
   while (!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground(GetColor(0x222222ff));
-    DrawFPS(10, 10);
+
     rlImGuiBegin();
+    ImGui::PushFont(font1, 0.0f);
 
-    // ImGuiID dockspace_id = ImGui::GetID("MyDockspace");
-    ImGui::DockSpaceOverViewport(0, ImGui::GetMainViewport(), ImGuiDockNodeFlags_PassthruCentralNode);
-    ImGui::DockSpace(ImGui::GetID("Dockspace"));
+    ImGuiID dockspace_id = ImGui::GetID("My Dockspace");
+    ImGuiViewport *viewport = ImGui::GetMainViewport();
 
-    ImGui::Begin("hello");
-    ImGui::Text("hai");
+    ImGuiWindowClass window_class_horizontal;
+    window_class_horizontal.DockNodeFlagsOverrideSet = ImGuiDockNodeFlags_NoTabBar | ImGuiDockNodeFlags_NoResizeY;
+
+    ImGuiWindowClass window_class_vertical;
+    window_class_vertical.DockNodeFlagsOverrideSet = ImGuiDockNodeFlags_NoTabBar | ImGuiDockNodeFlags_NoResizeX;
+
+    // Create settings
+    if (ImGui::DockBuilderGetNode(dockspace_id) == nullptr) {
+      ImGui::DockBuilderAddNode(dockspace_id, ImGuiDockNodeFlags_DockSpace);
+      ImGui::DockBuilderSetNodeSize(dockspace_id, viewport->Size);
+      ImGuiID dock_id_menu = 0;
+      ImGuiID dock_id_main = dockspace_id;
+      ImGui::DockBuilderSplitNode(dock_id_main, ImGuiDir_Up, 30 * (1.0/viewport->Size.y), &dock_id_menu, &dock_id_main);
+      ImGuiID dock_id_left = 0;
+      ImGui::DockBuilderSplitNode(dock_id_main, ImGuiDir_Left, 0.30f, &dock_id_left, &dock_id_main);
+      ImGui::DockBuilderDockWindow("Menu", dock_id_menu);
+      ImGui::DockBuilderDockWindow("Entries", dock_id_left);
+      ImGui::DockBuilderDockWindow("Content", dock_id_main);
+      ImGui::DockBuilderFinish(dockspace_id);
+    }
+
+    // Submit dockspace
+    ImGui::DockSpaceOverViewport(dockspace_id, viewport, ImGuiDockNodeFlags_PassthruCentralNode);
+
+    ImGui::SetNextWindowClass(&window_class_horizontal);
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0,0));
+    ImGui::Begin("Menu", NULL, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize);
+    ImGui::Button("Otwórz");
+    ImGui::SameLine();
+    ImGui::Button("Zapisz");
+    ImGui::End();
+    ImGui::PopStyleVar(1);
+
+    ImGui::SetNextWindowClass(&window_class_horizontal);
+    ImGui::Begin("Entries", NULL, ImGuiWindowFlags_NoMove);
     ImGui::End();
 
-    // ImGui::SetNextWindowPos(ImVec2(0,0));
-    // ImGui::SetNextWindowSize(ImVec2(GetScreenWidth(), GetScreenHeight()));
-    // ImGui::Begin("window", NULL, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoTitleBar);
-    //
-    // ImGui::BeginGroup();
-    // ImGui::EndGroup();
-    //
-    // ImGui::Separator();
-    //
-    // ImGui::BeginGroup();
-    // ImGui::EndGroup();
-    //
-    // ImGui::End();
+    ImGui::SetNextWindowClass(&window_class_horizontal);
+    ImGui::Begin("Content", NULL, ImGuiWindowFlags_NoMove);
+    ImGui::End();
 
+    ImGui::PopFont();
     rlImGuiEnd();
+
     EndDrawing();
   }
   rlImGuiShutdown();
